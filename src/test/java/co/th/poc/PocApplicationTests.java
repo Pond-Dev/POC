c1a6cb6919b79d20c83d4981fc28ef5d317c376f
Pond <sanhapas.arun@gmail.com>
2026-02-08 18:25:31 +0700

    init project

---
+++ src/test/java/co/th/poc/PocApplicationTests.java
@@ -0,0 +1,13 @@
+package co.th.poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
